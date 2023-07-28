9f5994269814c0ae3f75b6ca467959a72975d2af
Christian Junior Braffo <104594191+christianjunior05@users.noreply.github.com>
2023-07-28 10:25:08 +0000

    ajout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { InscriptionComponent } from './inscription/inscription.component';
+
+const routes: Routes = [
+  {path:"inscription" , component:InscriptionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
