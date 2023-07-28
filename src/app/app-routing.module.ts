72b26d5f7c79e91b13e1ff954a44d55a194ea879
Yann axel <ahouansouyann@ebenyx.com>
2023-07-28 10:20:47 +0000

    Architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {VerifyEmailComponent} from "./component/verify-email/verify-email.component";
+import {LoginComponent} from "./component/login/login.component";
+import {RegisterComponent} from "./component/register/register.component";
+import {ForgotPasswordComponent} from "./component/forgot-password/forgot-password.component";
+import {DashboardComponent} from "./component/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component : LoginComponent},
+  {path: 'dashboard', component : DashboardComponent},
+  {path: 'register', component : RegisterComponent},
+  {path: 'verify-email', component : VerifyEmailComponent},
+  {path: 'forgot-password', component : ForgotPasswordComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
