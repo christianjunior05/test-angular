92ba191b13c45a256268e344b649d9c9385d5d5f
Christian Junior Braffo <104594191+christianjunior05@users.noreply.github.com>
2023-08-01 19:20:48 +0000

    ajout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
-  production: true ,
-  firebase: {
-    apiKey: 'AIzaSyBFBg6ZkQxi0TwH2EeRLDkuM5a_66E6b9g',
-    authDomain: 'entrepot-d42b0.firebaseapp.com',
-    databaseURL: 'https:project.firebaseio.com',
-    projectId: 'entrepot-d42b0',
-    storageBucket: 'entrepot-d42b0.appspot.com',
-    messagingSenderId: '...',
-    appId: "1:415802477563:web:a1cbd5207372010766195e"
-  },
+  production: true 
+
 };
